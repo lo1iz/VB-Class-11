4b4eed89360c69d6ffb498114521d8cec48c5f21
Tanawat chinprapinporn <tanawatz@hotmail.com>
2019-02-05 15:35:03 +0700

    Completed

---
+++ MiniMartStock/MiniMartStock/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class minimart
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtUnit = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbID = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.gridData = New System.Windows.Forms.DataGridView()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.gridData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtPrice)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtUnit)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.cmbID)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(23, 21)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(297, 180)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "ข้อมูลสินค้า"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(173, 139)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(63, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "บาท (ต่อชิ้น)"
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(102, 136)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(59, 20)
+        Me.txtPrice.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(29, 139)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(56, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "ราคาสินค้า"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(173, 104)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(22, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "ชิ้น"
+        '
+        'txtUnit
+        '
+        Me.txtUnit.Location = New System.Drawing.Point(102, 101)
+        Me.txtUnit.Name = "txtUnit"
+        Me.txtUnit.Size = New System.Drawing.Size(59, 20)
+        Me.txtUnit.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "จำนวนสินค้า"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(102, 66)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(169, 20)
+        Me.txtName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "ชื่อสินค้า"
+        '
+        'cmbID
+        '
+        Me.cmbID.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbID.FormattingEnabled = True
+        Me.cmbID.Location = New System.Drawing.Point(102, 30)
+        Me.cmbID.Name = "cmbID"
+        Me.cmbID.Size = New System.Drawing.Size(93, 21)
+        Me.cmbID.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "รหัสสินค้า"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(336, 27)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(101, 29)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "เพิ่มข้อมูลสินค้า"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(336, 67)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(101, 29)
+        Me.btnEdit.TabIndex = 2
+        Me.btnEdit.Text = "แก้ไขข้อมูลสินค้า"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(336, 109)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(101, 29)
+        Me.btnReset.TabIndex = 3
+        Me.btnReset.Text = "ล้างข้อมูล"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'gridData
+        '
+        Me.gridData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.gridData.Location = New System.Drawing.Point(23, 221)
+        Me.gridData.Name = "gridData"
+        Me.gridData.Size = New System.Drawing.Size(414, 182)
+        Me.gridData.TabIndex = 4
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.BackgroundImage = Global.MiniMartStock.My.Resources.Resources.refresh
+        Me.btnRefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnRefresh.Location = New System.Drawing.Point(394, 175)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(43, 40)
+        Me.btnRefresh.TabIndex = 5
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'minimart
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(462, 423)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.gridData)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "minimart"
+        Me.Text = "MiniMart Stock"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.gridData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtUnit As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbID As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents gridData As DataGridView
+    Friend WithEvents btnRefresh As Button
+End Class
---
+++ MiniMartStock/MiniMartStock/Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class minimart
+    Dim conStr As String = "Server=(LocalDB)\MSSQLLocalDB;AttachDBFilename=|DataDirectory|\Minimart.mdf"
+    Dim conn As New SqlConnection(conStr)
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        conn.Open()
+        Dim sql As String = "INSERT INTO products(productid,
+                                                productname,
+                                                unit,
+                                                priceperunit)
+                             values ('','','','')"
+        Showdata()
+        conn.Close()
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        If cmbID.SelectedIndex = -1 Then
+            MessageBox.Show("กรุณาเลือกสินค้าที่ต้องการแก้ไข")
+        Else
+            conn.Open()
+            Dim sql As String = "UPDATE products
+                                SET productname = @proname,
+                                    priceperunit = @price,
+                                    unit = @unit
+                                where productid = @proid"
+            Dim cmd As New SqlCommand(sql, conn)
+            cmd.Parameters.AddWithValue("proname", txtName.Text)
+            cmd.Parameters.AddWithValue("price", txtPrice.Text)
+            cmd.Parameters.AddWithValue("unit", txtUnit.Text)
+            cmd.Parameters.AddWithValue("proid", cmbID.Text)
+            If cmd.ExecuteNonQuery = 1 Then
+                MessageBox.Show("แก้ไขข้อมูลเรียบร้อย", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("ไม่สามารถแก้ไขข้อมูลได้", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+            conn.Close()
+
+            Resetdata()
+
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Resetdata()
+    End Sub
+
+    Private Sub Resetdata()
+        txtName.Clear()
+        txtPrice.Clear()
+        txtUnit.Clear()
+        cmbID.Items.Clear()
+        Showdata()
+    End Sub
+
+    Private Sub gridData_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles gridData.CellContentClick
+
+    End Sub
+
+    Private Sub minimart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Showdata()
+    End Sub
+
+    Private Sub Showdata()
+        conn.Open()
+        Dim sql As String = "SELECT * FROM products"
+        Dim cmd As New SqlCommand(sql, conn)
+        Dim adapter As New SqlDataAdapter(cmd)
+        Dim data As New DataSet()
+        adapter.Fill(data, "Product")
+
+        gridData.DataSource = data.Tables("Product")
+
+        For i = 0 To data.Tables("Product").Rows.Count - 1
+            cmbID.Items.Add(data.Tables("Product").Rows(i)("productid"))
+        Next
+
+        conn.Close()
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        cmbID.Items.Clear()
+        Showdata()
+    End Sub
+
+    Private Sub cmbID_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbID.SelectedIndexChanged
+        If cmbID.SelectedIndex <> -1 Then
+            conn.Open()
+            Dim sql As String = "SELECT * FROM products
+                                 where productid = @proid"
+            Dim cmd As New SqlCommand(sql, conn)
+            cmd.Parameters.AddWithValue("proid", cmbID.Text)
+
+            Dim adapter As New SqlDataAdapter(cmd)
+            Dim data As New DataSet()
+            adapter.Fill(data, "Product")
+
+            txtName.Text = data.Tables("Product").Rows(0)("productname")
+            txtUnit.Text = data.Tables("Product").Rows(0)("Unit")
+            txtPrice.Text = data.Tables("Product").Rows(0)("priceperunit")
+            conn.Close()
+        End If
+    End Sub
+End Class
